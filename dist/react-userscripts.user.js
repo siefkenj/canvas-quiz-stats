c5034a1caa94b6249aabac85c72644a1874e19a8
Jason Siefken <siefkenj@gmail.com>
2020-11-25 18:01:39 -0500

    Updated version

--- dist/react-userscripts.user.js
+++ dist/react-userscripts.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     Canvas Quiz Stats
 // @namespace https://github.com/siefkenj/crowdmark-addons
-// @version  1.2
+// @version  1.3
 // @description A sample userscript built using react
 // @include https://q.utoronto.ca/courses/*
 // @grant    none
